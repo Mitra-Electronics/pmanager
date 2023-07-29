fdc81fb55e7bf551388b4dedaaf286696e2764ac
Mitra-Electronics <ishanmitra020@gmail.com>
2023-07-29 23:26:32 +0530

    v0.02

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './routes/Home.tsx'
 import Contact from './routes/Contact.tsx'
 import ErrorPage from './essentials/Error.tsx'
+import { QueryClientProvider, QueryClient} from '@tanstack/react-query'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     errorElement: <ErrorPage />,
   },
   {
     path: "people/:peopleId",
-    element: <Contact/>,
+    element: <Contact />,
   },
 ]);
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,19 +1,81 @@
 import Navbar from "../components/Navbar"
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query"
 
-const Contact = () =>{
+interface Person {
+    first_name: string,
+    last_name: string,
+    email: string,
+    phone: string,
+    birthday: string,
+    label: string,
+    twitter: string,
+    instagram: string,
+    github: string
+}
+
+interface Result {
+    success: boolean,
+    result: Person
+}
+
+const getContact = async (id: number):Promise<Person|undefined> => {
+    try {
+        const response = await axios.get("http://127.0.0.1:8000/get/id",
+        {
+            params: {
+                id: id
+            }
+        });
+        console.log(response);
+        const data:Result =  response.data
+        return data.result
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const Contact = () => {
     const { peopleId } = useParams()
+
+    const { data, status } = useQuery({
+        //    ^? const data: number | undefined
+        queryKey: ['test'],
+        queryFn: () => getContact((peopleId as unknown as number)),
+    })
+
+    if (status === "loading"){
+        return(
+            <>
+            <Navbar/>
+            <h1 className="text-xl text-white">Loading...</h1>
+            </>
+        )
+    }
+
+    else if (status === "error" || data === undefined){
+        return(
+            <>
+            <Navbar/>
+            <h1 className="text-xl text-white">Error</h1>
+            </>
+        )
+    }
+
     return (
         <>
-        <Navbar/>
-        <div className="flex items-center justify-center">
-        <div className="avatar">
-            <div className="mask rounded-full w-45 h-45">
-              <img src="https://lh3.googleusercontent.com/ogw/AGvuzYYndjzvnqcnojHTE1WBhsy9TaZbuMxZR9hpRv5ZZQ=s320-c-mo" alt="Avatar Tailwind CSS Component" />
+            <Navbar />
+            <div className="flex items-center justify-center">
+                <div className="avatar">
+                    <div className="mask rounded-full w-45 h-45">
+                        <img src="https://lh3.googleusercontent.com/ogw/AGvuzYYndjzvnqcnojHTE1WBhsy9TaZbuMxZR9hpRv5ZZQ=s320-c-mo" alt="Avatar Tailwind CSS Component" />
+                    </div>
+                    <h1 className="text-3xl text-white">{data.first_name} {data.last_name}</h1>
+                    <h1 className="text-xl">{data.email}</h1>
+                    <a href={"https://github.com/" + data.github}>hjk</a>
+                </div>
             </div>
-            <h1 className="text-xl">{peopleId}</h1>
-        </div>
-        </div>
         </>
     )
 }
